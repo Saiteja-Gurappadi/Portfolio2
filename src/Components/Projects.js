3acaa9fbedd7293e330835695f524078a4dae1ab
Saiteja-Gurappadi <saitejagvs8@gmail.com>
2025-09-27 20:43:18 +0530

    )

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,25 +5,38 @@ const Projects = () => {
     <div className="h-[100%] bg-gradient-to-tr from-black via-slate-900 to-black text-white p-[30px] flex flex-col items-center gap-[60px]">
       <h1 className="text-4xl ">My Projects</h1>
       <div className="grid grid-cols-2 gap-[10px]">
-        <div className="bg-gray-700/30 h-[500px] w-[320px] rounded-3xl backdrop-blur-md grid grid-rows-2 overflow-hidden">
+        
+        <div className="bg-gray-700/30 h-[500px] w-[320px] rounded-3xl backdrop-blur-md flex flex-col overflow-hidden">
           <img
             src={Weather1avif}
             alt=""
-            className="bg-cover bg-origin-padding w-full h-[190px] "
+            className="bg-cover bg-origin-padding w-full h-[310px] "
           />
-          <div className="bg-gray-100">
-            <h1>Weather</h1>
-            <p>
+          <div className="w-full h-full flex flex-col justify-center p-3">
+            <h1 className="text-xl font-bold ">Weather</h1>
+            <p className="text-slate-400 font-thin mb-[18px] mt-[4px] text-[15px]">
               Developed a responsive weather application in React.js that
               fetches and displays real-time weather data from an external API,
               allowing users to search for weather by city or use geolocation
               for automatic location detection.
             </p>
-            <div className="grid grid-cols-2">
-              <p>React Js</p>
-              <p>Tailwind Css</p>
-              <p>REST API</p>
-              <p>API Integration</p>
+            <div className="flex flex-col gap-[10px] ">
+              <div className="flex  gap-[30px]">
+                <p className="p-[10px] bg-gray-800 w-[100px] rounded-xl text-center shadow-xl">
+                  React Js
+                </p>
+                <p className="p-[10px] bg-gray-800 w-[150px] rounded-xl text-center shadow-xl ">
+                  Tailwind Css
+                </p>
+              </div>
+              <div className="flex gap-[15px]">
+                <p className="p-[10px] bg-gray-800 w-[100px] rounded-xl text-center shadow-xl ">
+                  REST API
+                </p>
+                <p className="p-[10px] bg-gray-800 w-[150px] rounded-xl text-center shadow-xl">
+                  API Integration
+                </p>
+              </div>
             </div>
           </div>
         </div>
