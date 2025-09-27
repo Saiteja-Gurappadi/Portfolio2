2510d4b5a213150f81632aa3e842fa9276973a49
Saiteja-Gurappadi <saitejagvs8@gmail.com>
2025-09-27 21:31:02 +0530

    m

---
+++ src/Components/MyProjectsLists.js
@@ -0,0 +1,22 @@
+const MyProjectsLists = [
+  {
+    id: 1,
+    projectname: "Weather",
+    projectsummary: "Developed a responsive weather application in React.js that fetches and displays real-time weather data from an external API, allowing users to search for weather by city or use geolocation for automatic location detection.",
+    skills:['React JS','Tailwind CSS','REST API' ,'API Integration'],
+    img:require('../Images/Weather1.avif'),
+    codelink:"",
+    website:"",
+  },
+  {
+    id: 2,
+    projectname: "Travel Tours",
+    projectsummary: "Developed a travel booking application with features like tour listing, booking, and user authentication.Integrated state management for seamless data flow using Redux.Enhanced the UI/UX with responsive design for an optimal user experience",
+    skills:['React JS','Tailwind CSS','REST API' ,'API Integration'],
+    img:require('../Images/BG1.jpg'),
+    codelink:"",
+    website:"",
+  }
+]
+
+export default MyProjectsLists;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,45 +1,42 @@
-import React from "react";
-import Weather1avif from "../Images/Weather1.avif";
+import MyProjectsLists from "../Components/MyProjectsLists";
 const Projects = () => {
   return (
     <div className="h-[100%] bg-gradient-to-tr from-black via-slate-900 to-black text-white p-[30px] flex flex-col items-center gap-[60px]">
       <h1 className="text-4xl ">My Projects</h1>
-      <div className="grid grid-cols-2 gap-[10px]">
-        
-        <div className="bg-gray-700/30 h-[500px] w-[320px] rounded-3xl backdrop-blur-md flex flex-col overflow-hidden">
-          <img
-            src={Weather1avif}
-            alt=""
-            className="bg-cover bg-origin-padding w-full h-[310px] "
-          />
-          <div className="w-full h-full flex flex-col justify-center p-3">
-            <h1 className="text-xl font-bold ">Weather</h1>
-            <p className="text-slate-400 font-thin mb-[18px] mt-[4px] text-[15px]">
-              Developed a responsive weather application in React.js that
-              fetches and displays real-time weather data from an external API,
-              allowing users to search for weather by city or use geolocation
-              for automatic location detection.
-            </p>
-            <div className="flex flex-col gap-[10px] ">
-              <div className="flex  gap-[30px]">
-                <p className="p-[10px] bg-gray-800 w-[100px] rounded-xl text-center shadow-xl">
-                  React Js
-                </p>
-                <p className="p-[10px] bg-gray-800 w-[150px] rounded-xl text-center shadow-xl ">
-                  Tailwind Css
-                </p>
-              </div>
-              <div className="flex gap-[15px]">
-                <p className="p-[10px] bg-gray-800 w-[100px] rounded-xl text-center shadow-xl ">
-                  REST API
-                </p>
-                <p className="p-[10px] bg-gray-800 w-[150px] rounded-xl text-center shadow-xl">
-                  API Integration
-                </p>
+      <div className="grid grid-cols-2 gap-[25px]">
+        {MyProjectsLists.map((data) => (
+          <div className="bg-gray-700/30 h-[500px] w-[320px] rounded-3xl backdrop-blur-md flex flex-col overflow-hidden">
+            <img
+              src={data.img}
+              alt=""
+              className="bg-cover bg-origin-padding w-full h-[310px] "
+            />
+            <div className="w-full h-full flex flex-col justify-center p-3">
+              <h1 className="text-xl font-bold ">{data.projectname}</h1>
+              <p className="text-slate-400 font-thin mb-[18px] mt-[4px] text-[15px]">
+                {data.projectsummary}
+              </p>
+              <div className="flex flex-col gap-[10px] ">
+                <div className="flex  gap-[30px]">
+                  <p className="p-[10px] bg-gray-800 w-[100px] rounded-xl text-center shadow-xl">
+                    React Js
+                  </p>
+                  <p className="p-[10px] bg-gray-800 w-[150px] rounded-xl text-center shadow-xl ">
+                    Tailwind Css
+                  </p>
+                </div>
+                <div className="flex gap-[15px]">
+                  <p className="p-[10px] bg-gray-800 w-[100px] rounded-xl text-center shadow-xl ">
+                    REST API
+                  </p>
+                  <p className="p-[10px] bg-gray-800 w-[150px] rounded-xl text-center shadow-xl">
+                    API Integration
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
